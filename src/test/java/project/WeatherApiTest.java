4d27b5a2a92f04d4c6120b98e77545a12f3af991
KarelVux <70542555+KarelVux@users.noreply.github.com>
2020-12-10 00:12:21 +0200

    REQ 1: Add refactor

--- src/test/java/project/WeatherApiTest.java
+++ src/test/java/project/WeatherApiTest.java
@@ -2,15 +2,22 @@ package project;
 
 import ee.icd0004.project.api.WeatherApi;
 import ee.icd0004.project.api.model.CurrentWeatherData;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WeatherApiTest {
+    private static WeatherApi weatherApi;
+
+    @BeforeClass
+    public static void setUp() {
+        weatherApi = new WeatherApi();
+    }
+
     @Test
     public void should_have_connection_from_owm_api() {
         String city = "Narva";
-        WeatherApi weatherApi = new WeatherApi();
 
         CurrentWeatherData currentWeatherData = weatherApi.getCurrentWeatherData(city);
 
