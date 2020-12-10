c7c373feafd97f8d35fe57161b90119d238d9511
KarelVux <70542555+KarelVux@users.noreply.github.com>
2020-12-10 02:09:04 +0200

    REQ 3: Add failing test

---
+++ src/test/java/ee/icd0004/project/CurrentWeatherReportTest.java
@@ -0,0 +1,17 @@
+package ee.icd0004.project;
+
+import ee.icd0004.project.model.CurrentWeatherReport;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CurrentWeatherReportTest {
+    @Test
+    public void should_have_date_in_correct_format_when_given_unix_timestamp() {
+        CurrentWeatherReport currentWeatherReport = new CurrentWeatherReport();
+
+        currentWeatherReport.setDate(1607558698);
+
+        assertThat(currentWeatherReport.getDate()).isEqualTo("2020-12-10");
+    }
+}
--- tests.md
+++ tests.md
@@ -1,2 +1,3 @@
 1. Get data from OWM api
 2. Current weather report should contain data
+3. Current weather report should return date in correct format when given unix timestamp
