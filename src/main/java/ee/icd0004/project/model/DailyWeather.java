96ab7242ac7c495c4f8c0fe39622b524f6a57f19
KarelVux <70542555+KarelVux@users.noreply.github.com>
2020-12-12 01:16:37 +0200

    REQ 11: Add implementation

--- src/main/java/ee/icd0004/project/model/DailyWeather.java
+++ src/main/java/ee/icd0004/project/model/DailyWeather.java
@@ -1,11 +1,17 @@
 package ee.icd0004.project.model;
 
+import ee.icd0004.project.util.UnixTimeStampConverter;
 import lombok.Data;
 
 @Data
 public class DailyWeather {
-    private Integer date;
+    private String date;
     private Double temperature;
     private Double humidity;
     private Double pressure;
+
+    public void setDate(Integer date) {
+        UnixTimeStampConverter unixTimeStampConverter = new UnixTimeStampConverter();
+        this.date = unixTimeStampConverter.getDateAsString(date);
+    }
 }
