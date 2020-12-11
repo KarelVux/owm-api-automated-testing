a5e2deb9f1c7816ffab47a7dda3a5bd20897efbe
KarelVux <70542555+KarelVux@users.noreply.github.com>
2020-12-11 17:21:56 +0200

    REQ 8: Add refactor

--- src/test/java/ee/icd0004/project/mock/WeatherTimeMockTest.java
+++ src/test/java/ee/icd0004/project/mock/WeatherTimeMockTest.java
@@ -26,24 +26,30 @@ public class WeatherTimeMockTest {
     public void should_have_main_details_block_in_weather_report() {
         String city = "Tallinn";
         WeatherTime weatherTime = new WeatherTime(weatherApi);
-        CurrentWeatherData currentWeatherData = new CurrentWeatherData();
-        currentWeatherData.setName(city);
-        currentWeatherData.setDt(1607698382 );
-        Coordiantes coordiantes = new Coordiantes();
-        coordiantes.setLon(24.75);
-        coordiantes.setLat(59.44);
-        currentWeatherData.setCoord(coordiantes);
-        Main main = new Main();
-        main.setTemp(-2.46);
-        main.setHumidity(92.);
-        main.setPressure(1016.0);
-        currentWeatherData.setMain(main);
-
+        CurrentWeatherData currentWeatherData = getMockedCurrentWeatherData(city);
 
         when(weatherApi.getCurrentWeatherData(anyString())).thenReturn(currentWeatherData);
         when(weatherApi.getUnits()).thenReturn("Metric");
         WeatherReport weatherReport = weatherTime.getWeatherReportForCity(city);
 
         assertThat(weatherReport.getWeatherReportDetails()).hasNoNullFieldsOrProperties();
+    }
+
+    private CurrentWeatherData getMockedCurrentWeatherData(String city) {
+        CurrentWeatherData currentWeatherData = new CurrentWeatherData();
+        currentWeatherData.setName(city);
+        currentWeatherData.setDt(1607698382 );
+
+        Coordiantes coordiantes = new Coordiantes();
+        coordiantes.setLon(24.75);
+        coordiantes.setLat(59.44);
+        currentWeatherData.setCoord(coordiantes);
+
+        Main main = new Main();
+        main.setTemp(-2.46);
+        main.setHumidity(92.);
+        main.setPressure(1016.0);
+        currentWeatherData.setMain(main);
+        return currentWeatherData;
     }
 }
