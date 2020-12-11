8ef3e44bcc1f46c6d3a37b246dea1c12d7f8e5d4
KarelVux <70542555+KarelVux@users.noreply.github.com>
2020-12-12 01:35:02 +0200

    REQ 11: Add refactor

---
+++ src/main/java/ee/icd0004/project/model/AbstractDailyWeather.java
@@ -0,0 +1,17 @@
+package ee.icd0004.project.model;
+
+import ee.icd0004.project.util.UnixTimeStampConverter;
+import lombok.Data;
+
+@Data
+abstract class AbstractDailyWeather {
+    private String date;
+    private Double temperature;
+    private Double humidity;
+    private Double pressure;
+
+    public void setDate(Integer date) {
+        UnixTimeStampConverter unixTimeStampConverter = new UnixTimeStampConverter();
+        this.date = unixTimeStampConverter.getDateAsString(date);
+    }
+}
--- src/main/java/ee/icd0004/project/model/CurrentWeatherReport.java
+++ src/main/java/ee/icd0004/project/model/CurrentWeatherReport.java
@@ -1,17 +1,4 @@
 package ee.icd0004.project.model;
 
-import ee.icd0004.project.util.UnixTimeStampConverter;
-import lombok.Data;
-
-@Data
-public class CurrentWeatherReport {
-    private String date;
-    private Double temperature;
-    private Double humidity;
-    private Double pressure;
-
-    public void setDate(Integer date) {
-        UnixTimeStampConverter unixTimeStampConverter = new UnixTimeStampConverter();
-        this.date = unixTimeStampConverter.getDateAsString(date);
-    }
+public class CurrentWeatherReport extends AbstractDailyWeather{
 }
