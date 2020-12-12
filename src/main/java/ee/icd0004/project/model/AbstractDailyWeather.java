3d2eab2815bb1741943967963f21110911dfa588
KarelVux <70542555+KarelVux@users.noreply.github.com>
2020-12-12 03:32:02 +0200

    REQ 12: Add implementation

--- src/main/java/ee/icd0004/project/model/AbstractDailyWeather.java
+++ src/main/java/ee/icd0004/project/model/AbstractDailyWeather.java
@@ -3,6 +3,10 @@ package ee.icd0004.project.model;
 import ee.icd0004.project.util.UnixTimeStampConverter;
 import lombok.Data;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Data
 abstract class AbstractDailyWeather {
     private String date;
@@ -13,5 +17,13 @@ abstract class AbstractDailyWeather {
     public void setDate(Integer date) {
         UnixTimeStampConverter unixTimeStampConverter = new UnixTimeStampConverter();
         this.date = unixTimeStampConverter.getDateAsString(date);
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Date getDateAsDateType() throws ParseException {
+        return new SimpleDateFormat("yyyy-MM-dd").parse(date);
     }
 }
--- src/main/java/ee/icd0004/project/model/ForecastReport.java
+++ src/main/java/ee/icd0004/project/model/ForecastReport.java
@@ -2,9 +2,23 @@ package ee.icd0004.project.model;
 
 import lombok.Data;
 
+import java.text.ParseException;
+import java.util.Comparator;
 import java.util.List;
 
 @Data
 public class ForecastReport {
     private List<DailyWeather> dailyWeathers;
+
+    public void orderDailyWeathersByDate() {
+        dailyWeathers.sort(Comparator.comparing(dailyWeather -> {
+                    try {
+                        return dailyWeather.getDateAsDateType();
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+        ));
+    }
 }
