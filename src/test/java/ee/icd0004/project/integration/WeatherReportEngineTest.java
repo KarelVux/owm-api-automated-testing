95cb6f629526a88856ceefccff718a383bee173f
KarelVux <70542555+KarelVux@users.noreply.github.com>
2020-12-14 01:34:51 +0200

    REQ 21: Fix test case

--- src/test/java/ee/icd0004/project/integration/WeatherReportEngineTest.java
+++ src/test/java/ee/icd0004/project/integration/WeatherReportEngineTest.java
@@ -1,5 +1,7 @@
 package ee.icd0004.project.integration;
 
+import ee.icd0004.project.json.JsonHandler;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -8,14 +10,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.contentOf;
 
 public class WeatherReportEngineTest {
+    private static final String fileInputPath = "src/test/java/ee/icd0004/project/testFile/input/";
+    private static final String fileOutputPath = "src/test/java/ee/icd0004/project/testFile/output/";
+
     @Test
     public void create_weather_report_with_forecast_json_file() {
-        String fileOutputPath = "src/test/java/ee/icd0004/project/testFile/output/";
+        String fileName = "city_name.json";
         String cityName = "Tallinn";
         WeatherReportEngine weatherReportEngine = new WeatherReportEngine();
+        weatherReportEngine.setFileInputPath(fileInputPath);
+        weatherReportEngine.setFileOutputPath(fileOutputPath);
 
-        weatherReportEngine.createWeatherReportJsonFile(cityName);
-        String outputCityFilepath = fileOutputPath + cityName + ".json";
+        weatherReportEngine.createWeatherReportJsonFile(fileName);
+        String outputCityFilepath = fileOutputPath + fileName;
         File outputFileLocation = new File(outputCityFilepath);
 
         assertThat(contentOf(outputFileLocation))
