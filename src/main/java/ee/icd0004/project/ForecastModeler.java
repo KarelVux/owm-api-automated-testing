21f6162b4284052670a0dcf8edf50fe1bd016a9f
KarelVux <70542555+KarelVux@users.noreply.github.com>
2020-12-13 03:35:33 +0200

    REQ 13: Add refactor

--- src/main/java/ee/icd0004/project/ForecastModeler.java
+++ src/main/java/ee/icd0004/project/ForecastModeler.java
@@ -10,9 +10,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class ForecastModeler {
-    public List<DailyWeather> getFormattedForecastFor5Days(ForecastData forecastData) {
-        Map<String, DailyWeather> weatherMap = new HashMap<>();
+    private Map<String, DailyWeather> weatherMap = new HashMap<>();
+    private List<DailyWeather> allowedWeatherDates = new ArrayList<>();
 
+    public List<DailyWeather> getFormattedForecastFor5Days(ForecastData forecastData) {
         for (Forecast forecast : forecastData.getList()) {
             DailyWeather dailyWeather = new DailyWeather();
             dailyWeather.setDate(forecast.getDt());
@@ -23,25 +24,31 @@ public class ForecastModeler {
             dailyWeather.setHumidity(forecastMain.getHumidity());
 
             if (weatherMap.containsKey(dailyWeather.getDate())) {
-                DailyWeather weatherMapValue = weatherMap.get(dailyWeather.getDate());
-                weatherMapValue.setDate(dailyWeather.getDate());
-                weatherMapValue.setHumidity(dailyWeather.getHumidity());
-                weatherMapValue.setPressure(dailyWeather.getPressure());
-                weatherMapValue.setTemperature(dailyWeather.getTemperature());
+                initializeDailyWeatherValuesInsideMap(dailyWeather);
             } else {
                 weatherMap.put(dailyWeather.getDate(), dailyWeather);
             }
         }
+        removeDatesAccordingRules();
 
-        List<DailyWeather> allowedWeather = new ArrayList<>();
+        return allowedWeatherDates;
+    }
+
+    private void initializeDailyWeatherValuesInsideMap(DailyWeather dailyWeather) {
+        DailyWeather weatherMapValue = weatherMap.get(dailyWeather.getDate());
+        weatherMapValue.setDate(dailyWeather.getDate());
+        weatherMapValue.setHumidity(dailyWeather.getHumidity());
+        weatherMapValue.setPressure(dailyWeather.getPressure());
+        weatherMapValue.setTemperature(dailyWeather.getTemperature());
+    }
+
+    private void removeDatesAccordingRules() {
         String currentDate = getCurrentDate();
         for (String key : weatherMap.keySet()) {
-            if (!key.equals(currentDate)){
-                allowedWeather.add(weatherMap.get(key));
+            if (!key.equals(currentDate)) {
+                allowedWeatherDates.add(weatherMap.get(key));
             }
         }
-
-        return allowedWeather;
     }
 
     private String getCurrentDate() {
