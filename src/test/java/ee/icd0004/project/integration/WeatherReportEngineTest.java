3fb47dd5740808025d32a9e84eb35e5726c83c55
KarelVux <70542555+KarelVux@users.noreply.github.com>
2020-12-20 15:13:42 +0200

    REQ 22: Refactor

--- src/test/java/ee/icd0004/project/integration/WeatherReportEngineTest.java
+++ src/test/java/ee/icd0004/project/integration/WeatherReportEngineTest.java
@@ -1,10 +1,15 @@
 package ee.icd0004.project.integration;
 
 import ee.icd0004.project.WeatherReportEngine;
+import ee.icd0004.project.WeatherTime;
+import ee.icd0004.project.api.WeatherApi;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.contentOf;
@@ -13,13 +18,21 @@ public class WeatherReportEngineTest {
     private static final String fileInputPath = "src/test/java/ee/icd0004/project/testFile/input/";
     private static final String fileOutputPath = "src/test/java/ee/icd0004/project/testFile/output/";
 
+    private static WeatherReportEngine weatherReportEngine;
+
+    @BeforeClass
+    public static void setUp() {
+        weatherReportEngine = new WeatherReportEngine();
+        weatherReportEngine.setFileInputPath(fileInputPath);
+        weatherReportEngine.setFileOutputPath(fileOutputPath);
+
+    }
+
+
     @Test
     public void create_weather_report_with_forecast_json_file() throws IOException {
         String fileName = "city_name.json";
         String cityName = "Tallinn";
-        WeatherReportEngine weatherReportEngine = new WeatherReportEngine();
-        weatherReportEngine.setFileInputPath(fileInputPath);
-        weatherReportEngine.setFileOutputPath(fileOutputPath);
 
         weatherReportEngine.createWeatherReportJsonFile(fileName);
         String outputCityFilepath = fileOutputPath + cityName + ".json";
@@ -35,19 +48,20 @@ public class WeatherReportEngineTest {
     @Test
     public void create_weather_report_with_multiple_city_names_json_file() throws IOException {
         String fileName = "city_names_with_multiple_names.json";
-        String cityName = "Tallinn";
-        WeatherReportEngine weatherReportEngine = new WeatherReportEngine();
-        weatherReportEngine.setFileInputPath(fileInputPath);
-        weatherReportEngine.setFileOutputPath(fileOutputPath);
+        List<String> cityNames = Arrays.asList("Tallinn", "Tartu", "Keila", "Narva");
 
+        String outputPath = fileOutputPath + "multipleCities/";
+        weatherReportEngine.setFileOutputPath(outputPath);
         weatherReportEngine.createWeatherReportJsonFile(fileName);
-        String outputCityFilepath = fileOutputPath + cityName + ".json";
-        File outputFileLocation = new File(outputCityFilepath);
 
-        assertThat(contentOf(outputFileLocation))
-                .isNotNull()
-                .isNotEmpty()
-                .contains(cityName);
+        for (String city : cityNames) {
+            String outputCityFilepath = outputPath + city + ".json";
+            File outputFileLocation = new File(outputCityFilepath);
 
+            assertThat(contentOf(outputFileLocation))
+                    .isNotNull()
+                    .isNotEmpty()
+                    .contains(city);
+        }
     }
 }
